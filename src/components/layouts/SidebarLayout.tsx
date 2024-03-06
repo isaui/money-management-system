4e7c9bd68d2559f6a5dffc9d9a7cdde337165343
isaui <isadestroyed17@gmail.com>
2024-03-06 20:53:55 +0700

    fix!<layout>: remove overflow-x

--- src/components/layouts/SidebarLayout.tsx
+++ src/components/layouts/SidebarLayout.tsx
@@ -6,11 +6,11 @@ import Sidebar from "../elements/Sidebar";
 const SidebarWrapper: React.FC<{ children: React.ReactNode, sidebar: React.ReactNode }> = ({ children, sidebar }) => {
     const { isSidebarOpen} = useSidebar();
     return (
-        <div className="flex min-h-screen max-h-screen linear-gradient-indigo-to-purple">
+        <div className="flex min-h-screen max-h-screen max-w-[100vw] overflow-x-hidden linear-gradient-indigo-to-purple">
         <div className={`fixed top-0 left-0 z-20 md:relative   transition-all duration-300 ${isSidebarOpen? 'flex w-[20%] translate-x-0 min-w-[18rem]' : ' -translate-x-[24rem] md:translate-x-0 flex md:w-[6rem]' }   h-screen`}>
             {sidebar}
         </div>
-        <div className="grow overflow-y-auto">
+        <div className="grow overflow-y-auto overflow-x-hidden max-w-[100vw]">
           {children}
         </div>
       </div>
--- src/components/modules/DashboardModule/index.tsx
+++ src/components/modules/DashboardModule/index.tsx
@@ -10,12 +10,12 @@ import DontHaveAccess from "@/components/elements/DontHaveAccess"
 const DashboardModule = () =>{
     const {user} = useAuth();
     return <SidebarLayout>
-    <div className="flex flex-col  w-full h-full">
-    <div className="p-4 w-full flex flex-col h-full mx-auto">
+    <div className="flex flex-col  w-full h-full ">
+    <div className="p-4 w-full flex flex-col h-full mx-auto ">
        <div className="flex max-w-7xl  justify-center  mx-auto w-full">
         <Header includeSearchBar={true} includeSidebarMenu={true} />
         </div>
-        <div className="flex flex-col  w-full mx-auto overflow-y-auto pt-4">
+        <div className="flex flex-col  w-full mx-auto overflow-y-auto pt-4 ">
             <div className="flex w-full mx-auto max-w-7xl space-x-4 px-4 ">
             {
                 user? <div className="flex flex-col grow ">
